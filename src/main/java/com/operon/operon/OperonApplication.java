c3310038d503b9be220f1baef5f796cfbf8c0fe1
MrVolfex <mihailo538@gmail.com>
2026-05-09 10:23:14 +0200

    Initial commit

---
+++ src/main/java/com/operon/operon/OperonApplication.java
@@ -0,0 +1,13 @@
+package com.operon.operon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/operon/operon/OperonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operon.operon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
